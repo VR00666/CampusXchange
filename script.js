6645800956e9fa20921e656f3385c0b8c51e4d57
Vyshakh Rajesh <vyshakhrajesh@gmail.com>
2026-01-13 19:21:31 +0530

    Initial CampusXchange frontend

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+    /* ==========================
+       ADD ITEM FORM LOGIC
+    ========================== */
+// Firebase SDKs
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+
+
+// TODO: Replace with your Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyDzG06a06LaXvrLUd6jpDS-s7nYUF7k0KY",
+  authDomain: "campusxchange-81a70.firebaseapp.com",
+  projectId: "campusxchange-81a70",
+  storageBucket: "campusxchange-81a70.firebasestorage.app",
+  messagingSenderId: "232825369256",
+  appId: "1:232825369256:web:aa24a4d5325d15abd7489d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+console.log("🔥 Firebase connected successfully");
+
+   
